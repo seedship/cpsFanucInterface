ab74fbfe3ac0ee4551123e4b02a41c028c628168
Elias Dellago <eliasdellago@gmail.com>
2021-08-19 17:19:17 +0200

    file structure

---
+++ xml_endpoint/server.py
@@ -0,0 +1,17 @@
+# Echo server program
+import socket
+import time
+
+HOST = '127.0.0.1'
+PORT = 8000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+print('Server startup')
+
+s.listen(1)
+conn, addr = s.accept()
+print ('Connected by', addr)
+
+conn.sendall('Hello, AIsmart!'.encode())
+print ('Send message')
+conn.close()
