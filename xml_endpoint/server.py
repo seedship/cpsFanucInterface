80b5c8a895ea2ef1845c9d2a1cf5bca182efefe7
Elias Dellago <eliasdellago@gmail.com>
2021-08-19 18:34:04 +0200

    include xml parser in client

--- xml_endpoint/server.py
+++ xml_endpoint/server.py
@@ -8,10 +8,19 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 print('Server startup')
 
+xml_single_str = r'<?xml version="1.0" encoding="ASCII"?>' \
+                 r'<struct_tag int_val="10" float_val="-2.7" string_val="abc"/>'
+xml_varlist_str = r'<?xml version="1.0" encoding="ASCII"?>' \
+                  r'<list_tag>' \
+                  r'<struct_tag int_val="10" float_val="-2.7" string_val="abc"/>' \
+                  r'<struct_tag int_val="9" float_val="-2.6" string_val="def"/>' \
+                  r'<struct_tag int_val="8" float_val="-2.5" string_val="ghi"/>' \
+                  r'</list_tag>'
+
 s.listen(1)
 conn, addr = s.accept()
 print ('Connected by', addr)
 
-conn.sendall('Hello, AIsmart!'.encode("ascii"))
+conn.sendall(xml_varlist_str.encode("ascii"))
 print ('Send message')
-conn.close()
+# conn.close()
